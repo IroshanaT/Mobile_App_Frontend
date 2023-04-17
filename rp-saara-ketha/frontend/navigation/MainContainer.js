cf8782f139539cf40760ddd029e0dce597b082eb
IroshanaT <thamaliro97@gmail.com>
2023-04-18 03:07:24 +0530

    Component Updated

--- rp-saara-ketha/frontend/navigation/MainContainer.js
+++ rp-saara-ketha/frontend/navigation/MainContainer.js
@@ -1,22 +1,29 @@
-import React from "react";
+import * as React from "react";
 import { Text } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from "./screens/HomeScreen";
 
+
+import HomeScreen from "./screens/HomeScreen";
+import LocationTracking from "./screens/Location_Tracking/LocationTracking";
+
+//Screen names
+const home = "Home";
+const locationTracking = "LocationTracking";
+
+
+//Stack navigator
 const Stack = createNativeStackNavigator();
+
 
 export default function MainContainer() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Welcome" }}
-        />
+        <Stack.Screen name={home} component={HomeScreen} />
+        <Stack.Screen name={locationTracking} component={LocationTracking} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ rp-saara-ketha/frontend/navigation/screens/AboutUs.js
@@ -0,0 +1,28 @@
+import {
+    View,
+    Platform,
+    StyleSheet,
+    Text,
+    Alert,
+    Dimensions,
+    TouchableOpacity,
+    Image,
+  } from "react-native";
+  import {
+    NativeBaseProvider,
+    ScrollView,
+    Center,
+    Heading,
+    Button,
+    Box,
+    VStack,
+    FormControl,
+    Input,
+    Spacer,
+    Select,
+    CheckIcon,
+    Flex,
+    TextArea,
+  } from "native-base";
+
+
